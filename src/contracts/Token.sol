607b06083dcdb9537a397081fb8454877fc7472a
charlesmariga <charlesmariga37@gmail.com>
2022-06-15 19:33:05 +0300

    Updated exchange contracts

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,48 +4,69 @@ pragma solidity <0.9.0;
 import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
 
 contract Token {
-  using SafeMath for uint;
+    using SafeMath for uint256;
 
-  string public name = "DappToken";
-  string public symbol = "DAPP";
-  uint256 public decimals = 18;
-  uint256 public totalSupply;
-  mapping(address => uint256) public balanceOf;
-  mapping(address => mapping(address => uint256)) public allowance; // user->spender->amount
+    string public constant name = "DappToken";
+    string public constant symbol = "DAPP";
+    uint256 public constant decimals = 18;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance; // user->spender->amount
 
-  event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
-  constructor(uint256 _totalSupply) {
-    totalSupply = _totalSupply;
-    balanceOf[msg.sender] = _totalSupply;
-  }
+    constructor(uint256 _totalSupply) {
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
+    }
 
-  function _transfer(address _from, address _to, uint256 _value) internal {
-    require(_to != address(0));
-    balanceOf[_from] = balanceOf[_from].sub(_value);
-    balanceOf[_to] = balanceOf[_to].add(_value);
-    emit Transfer(_from, _to, _value);
-  }
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
+        require(_to != address(0));
+        balanceOf[_from] = balanceOf[_from].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(_from, _to, _value);
+    }
 
-  function transfer(address _to, uint256 _value) public returns (bool success) {
-    require(balanceOf[msg.sender] >= _value);
-    _transfer(msg.sender, _to, _value);
-    return true;
-  }
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
 
-  function approve(address _spender, uint256 _value) public returns (bool success) {
-    require(_spender != address(0));
-    allowance[msg.sender][_spender] = _value;
-    emit Approval(msg.sender, _spender, _value);
-    return true;
-  }
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_spender != address(0));
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
 
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) { 
-    require(_value <= balanceOf[_from]);
-    require(_value <= allowance[_from][msg.sender]);
-    allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
-    _transfer(_from, _to, _value);
-    return true;
-  }
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from], "Insufficient balance");
+        require(
+            _value <= allowance[_from][msg.sender],
+            "Insufficeint approved tokens"
+        );
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+        _transfer(_from, _to, _value);
+        return true;
+    }
 }
