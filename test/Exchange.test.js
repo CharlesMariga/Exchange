ea76ed7d2f06c319f74d5d02190f4c8fb416ef94
charlesmariga <charlesmariga37@gmail.com>
2022-07-06 18:06:14 +0300

    Implemented depositing and withdrawing

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -78,6 +78,50 @@ contract("Exchange", ([deployer, feeAccount, user1]) => {
     });
   });
 
+  describe("withdrawing ether", () => {
+    let result;
+    let amount = ether(1);
+
+    beforeEach(async () => {
+      await exchange.depositEther({ from: user1, value: amount });
+    });
+
+    describe("success", () => {
+      it("withdraws Ether funds", async () => {
+        result = await exchange.withdrawEther(amount, { from: user1 });
+        const balance = await exchange.tokens(ETHER_ADDRESS, user1);
+        balance.toString().should.equal("0");
+      });
+
+      it("emits a Withdraw event", async () => {
+        const {
+          event,
+          args: { _token, _user, _amount, _balance },
+        } = result.logs[0];
+        event.should.equal("Withdraw");
+        _token
+          .toString()
+          .should.equal(
+            ETHER_ADDRESS.toString(),
+            "token address is not correct"
+          );
+        _user.should.equal(user1, "user address is not correct");
+        _amount
+          .toString()
+          .should.equal(amount.toString(), "amount is not correct");
+        _balance.toString().should.equal("0", "balance is not correct");
+      });
+    });
+
+    describe("failure", () => {
+      it("rejects withdrawal for insufficient balances", async () => {
+        await exchange
+          .withdrawEther(ether(100), { from: user1 })
+          .should.be.rejectedWith(EVM_REVERT);
+      });
+    });
+  });
+
   describe("depositing tokens", () => {
     let amount = tokens(10);
     let result;
@@ -132,6 +176,69 @@ contract("Exchange", ([deployer, feeAccount, user1]) => {
         await exchange
           .depositToken(token.address, amount, { from: user1 })
           .should.be.rejectedWith(EVM_REVERT);
+      });
+    });
+  });
+
+  describe("withdrawing tokens", () => {
+    let result;
+    let amount = tokens(10);
+
+    beforeEach(async () => {
+      await token.approve(exchange.address, amount, { from: user1 });
+      await exchange.depositToken(token.address, amount, { from: user1 });
+      result = await exchange.withdrawToken(token.address, amount, {
+        from: user1,
+      });
+    });
+
+    describe("success", () => {
+      it("withdraws Token funds", async () => {
+        const balance = await exchange.tokens(token.address, user1);
+        balance.toString().should.equal("0");
+      });
+
+      it("emits a Withdraw event", async () => {
+        const {
+          event,
+          args: { _token, _user, _amount, _balance },
+        } = result.logs[0];
+        event.should.equal("Withdraw");
+        _token
+          .toString()
+          .should.equal(
+            token.address.toString(),
+            "token address is not correct"
+          );
+        _user.should.equal(user1, "user address is not correct");
+        _amount
+          .toString()
+          .should.equal(amount.toString(), "amount is not correct");
+        _balance.toString().should.equal("0", "balance is not correct");
+      });
+    });
+
+    describe("failure", () => {
+      it("rejects Ether withdrawal", async () => {
+        await exchange
+          .withdrawToken(ETHER_ADDRESS, amount, { from: user1 })
+          .should.be.rejectedWith(EVM_REVERT);
+      });
+
+      it("rejects withdrawal for insufficient balances", async () => {
+        await exchange
+          .withdrawToken(token.address, tokens(100), { from: user1 })
+          .should.be.rejectedWith(EVM_REVERT);
+      });
+    });
+
+    describe("checking balances", () => {
+      beforeEach(async () => [
+        await exchange.depositEther({ from: user1, value: ether(1) }),
+      ]);
+      it("returns user balance", async () => {
+        const balance = await exchange.balanceOf(ETHER_ADDRESS, user1);
+        balance.toString().should.equal(ether(1).toString());
       });
     });
   });
